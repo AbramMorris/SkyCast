c1c03d3af7540f6a8cca5002c7154abfa897693d
Abram morris <abram.morris62@gmail.com>
2025-03-13 00:28:24 +0200

    creating splashScreen

--- app/src/main/java/com/example/skycast/ui/theme/Theme.kt
+++ app/src/main/java/com/example/skycast/ui/theme/Theme.kt
@@ -12,15 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = BlueBlack
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = BlueLight
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -36,7 +32,6 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun SkyCastTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
