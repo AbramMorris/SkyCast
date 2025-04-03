186cc791855dd9d3edac9736dea2345c5c1e85a9
Abram morris <abram.morris62@gmail.com>
2025-04-03 08:47:54 +0200

    Refactor: Use coEvery and coVerify for testing AlarmRepoImp

--- app/src/test/java/com/example/skycast/data/repo/AlarmRepoImpTest.kt
+++ app/src/test/java/com/example/skycast/data/repo/AlarmRepoImpTest.kt
@@ -5,11 +5,7 @@ import com.example.skycast.data.models.AlarmEntity
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.test.setMain
-import org.junit.After
 import org.junit.Before
 import org.junit.Test
 class AlarmRepoImpTest {
@@ -19,33 +15,28 @@ class AlarmRepoImpTest {
 
     @Before
     fun setup() {
-        Dispatchers.setMain(Dispatchers.Unconfined)
         alarmRepo = AlarmRepoImp(localDataSource)
     }
     @Test
     fun test_insertAlarm_calls_localDataSource_insertAlarm_() = runTest {
-        // Arrange
+
         val alarm = AlarmEntity(1, 12, 22, "alarm", 0.0, 0.0)
-        // Mock the insertAlarm function so it does nothing
+
         coEvery { localDataSource.insertAlarm(any()) } returns Unit
-        // Act
+
         alarmRepo.insertAlarm(alarm)
-        // Assert
-        coVerify { localDataSource.insertAlarm(alarm) } // Verify that insertAlarm was called
+
+        coVerify { localDataSource.insertAlarm(alarm) }
     }
     @Test
     fun test_deleteAlarm_calls_localDataSource_deleteAlarm() = runTest {
-        // Arrange
+
         val alarm = AlarmEntity(1, 12, 22, "alarm", 0.0, 0.0)
-        // Mock the deleteAlarm function so it does nothing
+
         coEvery { localDataSource.deleteAlarm(any()) } returns Unit
-        // Act
+
         alarmRepo.deleteAlarm(alarm)
-        // Assert
-        coVerify { localDataSource.deleteAlarm(alarm) } // Verify that deleteAlarm was called
-    }
-    @After
-    fun tearDown() {
-        Dispatchers.resetMain()
+
+        coVerify { localDataSource.deleteAlarm(alarm) }
     }
 }
